a621fb49fab30a34a3b048edb4ed22680bb425e5
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-27 15:19:35 +0530

    all done

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-v
+let url = 'http://localhost:3000';
+
+
+let test = async ()=>{
+    // let res = await axios.post(`${url}/todo`,{
+    //     text: 'This is a test'
+    // });
+
+     let res = await axios.get(`${url}`);
+
+    // let res = await axios.delete(`${url}/delete/1`);
+    
+    // let res = await axios.put(`${url}/edit/2023-07-13T06:29:08.367Z`,{
+    //     text: 'Some new text'
+    // });
+
+
+    console.log(res);
+}
+test();
